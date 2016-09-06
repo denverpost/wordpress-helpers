84b82719781e7d9d40b83f5ea183b31c7b243016
freejoe76 <jmurphy@denverpost.com>
2016-09-06 13:18:37 -0500

    Write the article detail retrival function

--- in-article-promo/api.php
+++ in-article-promo/api.php
@@ -13,8 +13,13 @@ class Api {
         $this->items = json_decode($this->json);
     }
 
-    public function get() { 
-        return $this->json;
+    public function find($value, $field='url') { 
+        foreach ( $this->items as $key => $val ):
+           if ( $this->items[$key][$field] == $value ):
+                return $this->items[$key];
+            endif;
+        endforeach;
+        return False;
     }
 
     public function post() {
@@ -29,8 +34,9 @@ class Api {
 }
 
 class Article {
-    // Handle getting and storing detailed article information
+    // Handle retrieving detailed article information
     public $url;
+    public $article;
 
     public function __construct($url)
     {
@@ -38,20 +44,43 @@ class Article {
     }
 
     public function retrieve() {
+        // Download the markup, then process it.       
+        $url = str_replace('&amp;','&',$this->url);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $markup = curl_exec($ch);
+        curl_close($ch);
+
+        preg_match('/<meta property="og:title" content="([^"]+)" \/>/', $markup, $matches);
+        $this->article['title'] = trim($matches[1]);
+        preg_match('#<meta name="twitter:image" content="([^?]+)\?w=640"#', $markup, $matches);
+        $this->article['image'] = $matches[1];
+        preg_match("#'Section' : '([^']+)',#", $markup, $matches);
+        $this->article['section'] = $matches[1];
+
+        return $this->article;
     }
 
 }
 
 
 $articles_live = new Api('articles_live');
-$articles_details = new Api('articles_detail');
+$articles_detail = new Api('articles_detail');
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $url = htmlspecialchars($_POST['url']);
     $articles_working = $articles_live->items;
-    $articles_working[] = [
-        'id'      => round(microtime(true) * 1000),
-        'url'  => htmlspecialchars($_POST['url'])
-    ];
+
+    // See if we already have metadata for this URL in our details json file.
+    $detail = $articles_detail->find($url);
+    if ( $detail === False ):
+        $article = Article($url);
+        $detail = $article->retrieve();
+    endif;
+    $articles_working[] = $detail;
+
 
     $articles_live->items = $articles_working;
     $articles_live->json = json_encode($articles_working, JSON_PRETTY_PRINT);
@@ -61,3 +90,7 @@ header('Content-Type: application/json');
 header('Cache-Control: no-cache');
 header('Access-Control-Allow-Origin: *');
 echo $articles_live->json;
+
+function get_content($url)
+{
+}
