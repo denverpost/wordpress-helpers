fae719da55ab8fb01377201666b775a5d2739adb
freejoe76 <jmurphy@denverpost.com>
2016-09-12 12:15:24 -0600

    Fix syntax error

--- in-article-promo/deploy.bash
+++ in-article-promo/deploy.bash
@@ -4,8 +4,8 @@ python2.7 ingest.py http://www.denverpost.com/news/feed/ --slug hard-news
 python2.7 ingest.py http://www.denverpost.com/sports/feed/ --slug sports
 
 ../ftp.bash --dir $REMOTE_DIR --host $REMOTE_HOST
-for SECTION in 'dont-miss' 'sports' 'hard-news'; do:
-    for ITEM in {1..5}; do:
+for SECTION in dont-miss sports hard-news; do
+    for ITEM in {1..5}; do
         curl -X PURGE http://extras.denverpost.com/app/in-article-promo/$SECTION-$ITEM.html
     done
 done
