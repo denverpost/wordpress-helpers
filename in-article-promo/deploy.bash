5e108fbaf2d53983772919fb5a67b20deae521a8
freejoe76 <jmurphy@denverpost.com>
2016-09-12 12:30:09 -0600

    More verbose

--- in-article-promo/deploy.bash
+++ in-article-promo/deploy.bash
@@ -6,6 +6,8 @@ python2.7 ingest.py http://www.denverpost.com/sports/feed/ --slug sports
 ../ftp.bash --dir $REMOTE_DIR --host $REMOTE_HOST
 for SECTION in dont-miss sports hard-news; do
     for ITEM in {1..5}; do
-        curl -X PURGE http://extras.denverpost.com/app/in-article-promo/$SECTION-$ITEM.html
+        URL="http://extras.denverpost.com/app/in-article-promo/$SECTION-$ITEM.html"
+        echo $URL
+        curl -X PURGE $URL
     done
 done
