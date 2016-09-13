621e99d8dcf095c0f8f1e6565e69bc3abfa6356d
freejoe76 <joe.murphy@gmail.com>
2016-09-13 09:59:44 -0600

    Add Broncos promos

--- in-article-promo/deploy.bash
+++ in-article-promo/deploy.bash
@@ -2,9 +2,10 @@
 python2.7 ingest.py http://www.denverpost.com/dont-miss/feed/ --slug dont-miss
 python2.7 ingest.py http://www.denverpost.com/news/feed/ --slug hard-news
 python2.7 ingest.py http://www.denverpost.com/sports/feed/ --slug sports
+python2.7 ingest.py http://www.denverpost.com/sports/denver-broncos/feed/ --slug broncos 
 
 ../ftp.bash --dir $REMOTE_DIR --host $REMOTE_HOST
-for SECTION in dont-miss sports hard-news; do
+for SECTION in dont-miss sports hard-news broncos; do
     for ITEM in {1..5}; do
         URL="http://extras.denverpost.com/app/in-article-promo/$SECTION-$ITEM.html"
         echo $URL
